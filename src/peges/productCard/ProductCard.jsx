dd4ab433b5386453dbe00165889b115acd1a56d2
MariaaPcsa <marialujivan@gmail.com>
2025-05-29 09:31:25 -0300

    style

--- src/peges/productCard/ProductCard.jsx
+++ src/peges/productCard/ProductCard.jsx
@@ -3,12 +3,12 @@ import { faCartPlus } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 const ProductCard = ({ id, name, brand, price, image, feminine }) => {
   return (
-    <article className=" card-produto group w-48 bg-stone-100 shadow-xl shadow-slate-200 flex flex-col justify-around border-2 border-gray-200 rounded-lg w-full h-auto
+    <article className=" card-produto group w-48 bg-stone-100 shadow-xl shadow-slate-200 flex flex-col justify-around border-2 border-gray-200 rounded-lg sm:mx-4 md:mx-6
 ">
       <img
         src={image}
         alt={`Imagem do Produto ${id}`}
-        className=" group-hover:scale-110 rounded-lg mx-4 my-3 transition duration-300" 
+        className=" group-hover:scale-110 rounded-lg mx-4 my-3 transition duration-300  " 
       />
       <div className="p-4 flex flex-col gap-2"> 
         <p className="text-sm text-gray-500">{brand}</p>
