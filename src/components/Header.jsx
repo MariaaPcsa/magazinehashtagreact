76deab7d86b93814134ccfe4c2c654aaf775f503
MariaaPcsa <marialujivan@gmail.com>
2025-05-28 22:51:46 -0300

    style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
     <header className='  text-white flex text-xl sticky top-0 shadow-xl shadow-slete-400 bg-slate-950 text-slete-200 px-8 py-4 items-end justify-between text-base z-10'>
         
    <a href="/">
- <img className='h-16 px-2' src={logo} alt='Logamarca hashtag'/>
+ <img className='h-16 px-2 rounded-lg mx-4 my-3 ' src={logo} alt='Logamarca hashtag'/>
 
    </a>
 
--- src/components/UserButtons.jsx
+++ src/components/UserButtons.jsx
@@ -3,15 +3,18 @@ import { faUser, faCartShopping } from "@fortawesome/free-solid-svg-icons"
 
 const UserButtons = () => {
   return (
-    <div className="">
-      <button className="px-2  ">
+    <div className="flex gap-4 p-4">
+      <button className="flex items-center gap-2 px-4 py-2 bg-slate-700 text-white rounded transition duration-300 transform hover:scale-110 hover:bg-slate-500 shadow">
         Carrinho <FontAwesomeIcon icon={faCartShopping} />
       </button>
-      <a href="#" className=" px-2">
+      <a
+        href="#"
+        className="flex items-center gap-2 px-4 py-2 bg-slate-700 text-white rounded transition duration-300 transform hover:scale-110 hover:bg-slate-500 shadow"
+      >
         Histórico <FontAwesomeIcon icon={faUser} />
       </a>
     </div>
   )
 }
 
-export default UserButtons;
+export default UserButtons
