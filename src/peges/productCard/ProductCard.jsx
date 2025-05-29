3d5aa7d603c89c5ef0234646dffd3b11cfc2e16d
MariaaPcsa <marialujivan@gmail.com>
2025-05-29 10:00:56 -0300

    style

--- src/peges/productCard/ProductCard.jsx
+++ src/peges/productCard/ProductCard.jsx
@@ -3,16 +3,16 @@ import { faCartPlus } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 const ProductCard = ({ id, name, brand, price, image, feminine }) => {
   return (
-    <article className=" card-produto group w-48 bg-stone-100 shadow-xl shadow-slate-200 flex flex-col justify-around border-2 border-gray-200 rounded-lg sm:mx-4 md:mx-6
+    <article className=" card-produto group  w-48 bg-stone-100 shadow-xl shadow-slate-200 flex flex-col justify-around border-2 border-gray-200 rounded-lg sm:mx-4 md:mx-6  md:px-2 md:py-30
 ">
       <img
         src={image}
         alt={`Imagem do Produto ${id}`}
         className=" group-hover:scale-110 rounded-lg mx-4 my-3 transition duration-300  " 
       />
-      <div className="p-4 flex flex-col gap-2"> 
+      <div className="p-4 flex flex-col px-1 py-30"> 
         <p className="text-sm text-gray-500">{brand}</p>
-        <p className="text-lg font-semibold text-gray-800">{name}</p>
+        <p className=" text-lg font-semibold text-gray-800">{name}</p>
         <p className="text-base font-medium text-green-600">R$ {price}</p>
         <button className=' rounded-md mx-4 my-01 text-slate-100 bg-slate-950 group-hover:scale-110' >
          <FontAwesomeIcon icon={faCartPlus}/>  Adicionar</button>
